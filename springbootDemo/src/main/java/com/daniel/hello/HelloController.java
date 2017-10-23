2e501e1a256011e3f9e860eb54ea23ee02b26e04
DanielPei <peixq1222@icloud.com>
2017-10-23 15:48:06 +0800

    Config the swagger ui for restful controller.

--- springbootDemo/src/main/java/com/daniel/hello/HelloController.java
+++ springbootDemo/src/main/java/com/daniel/hello/HelloController.java
@@ -1,6 +1,9 @@
 package com.daniel.hello;
 
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 public class HelloController {
-	
-    @RequestMapping("/test")
+
+	@RequestMapping(method = RequestMethod.GET, value = "/test")
+	//	method : 指定对应Rest 的哪个方法；
+	//	value : 指定路径
+    @ApiOperation(value="API_Value", notes="API_Notes")
     public String index() {
         return "Greetings from Spring Boot!";
     }
